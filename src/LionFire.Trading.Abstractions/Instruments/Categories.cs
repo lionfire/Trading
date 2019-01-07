94d158e04f362a1612fca2fa60ccfa372a6ad608
Jared Thirsk <ja+commits@thirsk.ca>
2019-01-07 03:56:05 -0700

    snapshot

---
+++ src/LionFire.Trading.Abstractions/Instruments/Categories.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LionFire.Trading.Abstractions.Instruments
+{
+    public class MarketCategory
+    {
+        public string ShortCode { get; set; }
+        public string Name { get; set; }
+    }
+    public class Categories
+    {
+        public static IEnumerable<MarketCategory> Items => items;
+        private static List<MarketCategory> items;
+
+        static Categories()
+        {
+            items = new List<MarketCategory>();
+
+            items.Add(new MarketCategory { ShortCode = "B", Name = "Bonds" });
+            items.Add(new MarketCategory { ShortCode = "BM", Name = "Base Metals" });
+            items.Add(new MarketCategory { ShortCode = "CO", Name = "Commodities" });
+            items.Add(new MarketCategory { ShortCode = "CR", Name = "Cryptocurrencies" });
+            items.Add(new MarketCategory { ShortCode = "F", Name = "Forex" });
+            items.Add(new MarketCategory { ShortCode = "I", Name = "Indicies" });
+            items.Add(new MarketCategory { ShortCode = "PM", Name = "Precious Metals" });
+            items.Add(new MarketCategory { ShortCode = "S", Name = "Stocks" });
+        }
+    }
+}
---
+++ src/LionFire.Trading.Abstractions/Notifications/TPriceAlertType.cs
@@ -0,0 +1,14 @@
+﻿namespace LionFire.Trading.Notifications
+{
+    public enum TPriceAlertType
+    {
+        Bid = 0,
+        Ask,
+        Last,
+
+        /// <summary>
+        /// Average of Bid and ask
+        /// </summary>
+        Avg,
+    }
+}
