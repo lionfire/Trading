1fcc3b061f45aceea77d34e474c27c0bd87a9cd7
Jared Thirsk <ja+commits@thirsk.ca>
2019-04-14 21:30:59 -0600

    min/max with offset

---
+++ src/LionFire.Trading/Structures/DataSeriesExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+#if cTrader
+using cAlgo.API.Internals;
+#endif
+using System.Text;
+
+namespace LionFire.Trading
+{
+    
+    public static class DataSeriesExtensions
+    {
+        public static double MinimumOffset(this MarketSeries series, int periods, int offset = 2)
+        {
+            var s = series.Low;
+            double min = double.MaxValue;
+            for (int i = offset; i < periods; i++)
+            {
+                var val = s.Last(i);
+                if (val < min)
+                {
+                    min = val;
+                }
+            }
+            return min;
+        }
+        public static double MaximumOffset(this MarketSeries series, int periods, int offset = 2)
+        {
+            var s = series.High;
+            double max = double.MinValue;
+            for (int i = offset; i < periods; i++)
+            {
+                var val = s.Last(i);
+                if (val > max)
+                {
+                    max = val;
+                }
+            }
+            return max;
+        }
+    }
+}
