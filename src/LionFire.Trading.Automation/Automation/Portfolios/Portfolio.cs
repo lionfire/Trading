9726fcf22ffa0d32f1002f72a238793e6f0f4d50
Jared Thirsk <ja+commits@thirsk.ca>
2025-05-15 06:16:00 -0600

    progress

--- src/LionFire.Trading.Automation/Automation/Portfolios/Portfolio.cs
+++ src/LionFire.Trading.Automation/Automation/Portfolios/Portfolio.cs
@@ -1,64 +1,114 @@
-﻿using LionFire.FlexObjects;
+﻿using DynamicData;
+using LionFire.DynamicData_;
+using LionFire.FlexObjects;
 using LionFire.Referencing;
+using Microsoft.CodeAnalysis;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
-using DynamicData;
-using System.Reflection.Metadata.Ecma335;
-using ReactiveUI;
 
 namespace LionFire.Trading.Automation.Portfolios;
 
 [LionFire.Ontology.Alias("Portfolio")]
 public class Portfolio : ReactiveObject
 {
+    static ObjectIDGenerator generator = new ObjectIDGenerator();
+
+    public long InstanceId { get; set; }
+    public Portfolio()
+    {
+        InstanceId = generator.GetId(this, out bool firstTime);
+        if (firstTime)
+        {
+            Debug.WriteLine($"Portfolio {InstanceId} created");
+        }
+        else
+        {
+            Debug.WriteLine($"Portfolio {InstanceId} re-created");
+        }
+    }
+
     public string? Name { get; set; }
     public string? Comment { get; set; }
 
+    [Set]
+    public SourceCache<
+        (OptimizationRunReference key, SourceCache<BacktestReference, BacktestReference>), OptimizationRunReference>? OptimizationBacktestReferences
+    { get; set; }
+    object OptimizationBacktestReferencesLock = new();
 
     public SourceCache<
         (OptimizationRunReference key, SourceCache<(BacktestReference key, BacktestBatchJournalEntry value), BacktestReference> value), OptimizationRunReference>? OptimizationBacktests
     { get; set; }
+
     object OptimizationBacktestsLock = new();
 
     public void Toggle(OptimizationRunReference orr, BacktestBatchJournalEntry entry)
     {
-        this.RaisePropertyChanging(nameof(OptimizationBacktests));
+        bool embed = false;
 
-        lock (OptimizationBacktestsLock)
+        if (!embed)
         {
+            this.RaisePropertyChanging(nameof(OptimizationBacktestReferences));
 
-            BacktestReference r = entry;
-
-            if (OptimizationBacktests == null)
+            lock (OptimizationBacktestReferencesLock)
             {
-                OptimizationBacktests = new SourceCache<
-            (OptimizationRunReference key, SourceCache<(BacktestReference, BacktestBatchJournalEntry), BacktestReference>), OptimizationRunReference>(tuple => tuple.Item1);
-            }
+                OptimizationBacktestReferences ??= new(tuple => tuple.key);
 
-            SourceCache<(BacktestReference, BacktestBatchJournalEntry), BacktestReference> level1;
+                SourceCache<BacktestReference, BacktestReference> level1;
+                var level1O = OptimizationBacktestReferences.Lookup(orr);
+                if (level1O.HasValue) { level1 = level1O.Value.Item2; }
+                else
+                {
+                    level1 = new(x => x);
+                    OptimizationBacktestReferences.AddOrUpdate((orr, level1));
+                }
 
-            var level1O = OptimizationBacktests.Lookup(orr);
-            if (level1O.HasValue) { level1 = level1O.Value.Item2; }
-            else
-            {
-                level1 = new SourceCache<(BacktestReference key, BacktestBatchJournalEntry value), BacktestReference>(tuple => tuple.key);
-
-                OptimizationBacktests.AddOrUpdate((orr, level1));
+                BacktestReference r = entry;
+                var level2O = level1.Lookup(r);
+                if (level2O.HasValue) { level1.RemoveKey(r); }
+                else { level1.AddOrUpdate(r); }
             }
-
-            var level2O = level1.Lookup(r);
-            if (level2O.HasValue) { 
-                level1.RemoveKey(r);
-            }
-            else
-            {
-                level1.AddOrUpdate((r, entry));
-            }
+            Debug.WriteLine($"Portfolio RaisePropertyChanged - {InstanceId}");
+            this.RaisePropertyChanged(nameof(OptimizationBacktestReferences));
         }
-        this.RaisePropertyChanged(nameof(OptimizationBacktests));
+        else
+        {
+            this.RaisePropertyChanging(nameof(OptimizationBacktests));
+
+            lock (OptimizationBacktestsLock)
+            {
+                OptimizationBacktests ??= new(tuple => tuple.key);
+
+                SourceCache<(BacktestReference, BacktestBatchJournalEntry), BacktestReference> level1;
+                var level1O = OptimizationBacktests.Lookup(orr);
+                if (level1O.HasValue) { level1 = level1O.Value.value; }
+                else
+                {
+                    level1 = new SourceCache<(BacktestReference key, BacktestBatchJournalEntry value), BacktestReference>(tuple => tuple.key);
+
+                    OptimizationBacktests.AddOrUpdate((orr, level1));
+                }
+
+                BacktestReference r = entry;
+                var level2O = level1.Lookup(r);
+                if (level2O.HasValue)
+                {
+                    level1.RemoveKey(r);
+                }
+                else
+                {
+                    level1.AddOrUpdate((r, entry));
+                }
+            }
+            this.RaisePropertyChanged(nameof(OptimizationBacktests));
+        }
     }
     public bool IsInPortfolio(OptimizationRunReference orr, BacktestBatchJournalEntry entry)
     {
@@ -79,5 +129,10 @@ public class Portfolio : ReactiveObject
         var level2O = level1.Lookup(r);
         if (level2O.HasValue) { return true; }
         else { return false; }
+    }
+
+    public override string ToString()
+    {
+        return $"Portfolio {Name} #{InstanceId} ({OptimizationBacktestReferences?.KeyValues.SelectMany(kv=>kv.Value.Item2.Keys).Count()} backtest refs)";
     }
 }
