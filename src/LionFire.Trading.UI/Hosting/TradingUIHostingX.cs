88b9d45da5cfd3e0c3bb56ad5aff626ce6a7979e
Jared Thirsk <ja+commits@thirsk.ca>
2025-10-22 07:28:41 -0600

    Moved ObservableReader[Writer]VM.cs to LionFire.Data.Async.Mvvm

--- src/LionFire.Trading.UI/Hosting/TradingUIHostingX.cs
+++ src/LionFire.Trading.UI/Hosting/TradingUIHostingX.cs
@@ -15,6 +15,7 @@ public static class TradingUIHostingX
         return services
             .AddMvvm()
             .AddAsyncDataMvvm()
+            .AddReactivePersistenceMvvm()
             .AddTransient<ScannerVM>()
             ;
     }
